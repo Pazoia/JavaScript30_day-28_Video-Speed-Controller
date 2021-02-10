351c824576ae1dcd25363573bc94adcdd181698d
Paulo Azoia <pazoia.gomes@gmail.com>
2021-02-10 14:21:46 +0000

    Speed bar registering mousemove:
    also returning the percentage acording to where the mouse is in the bar

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const speed = document.querySelector('.speed');
+const bar = speed.querySelector('.speed-bar');
+const video = document.querySelector('.flex');
+
+function speedBarToggle(e) {
+    const y = e.pageY - this.offsetTop;
+    const percent = y / this.offsetHeight;
+    const min = 0.4;
+    const max = 4;
+    const height = Math.round(percent * 100) + '%';
+    console.log(height);
+}
+
+speed.addEventListener('mousemove', speedBarToggle);
